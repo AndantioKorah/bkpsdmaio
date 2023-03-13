58e9adc59c931aad7de6183c98d8740dc13d4725
Youri <torehyouri@gmail.com>
2023-03-13 16:01:18 +0800

    pull

--- application/views/Kepegawaian/V_UploadDokumen.php
+++ application/views/Kepegawaian/V_UploadDokumen.php
@@ -17,9 +17,7 @@
     <div class="col-12">
         
         <div class="card">
-            <div class="card-header">
-                <h5 class="card-title mb-0">Upload</h5>
-            </div>
+       
             <div class="card-body">
                 
             <form id="upload" action="<?php echo site_url()?>Kepegawaian/C_Kepegawaian/doUpload" class="dropzone">						
@@ -75,9 +73,9 @@
 <div class="row">
     <div class="col-12">
         <div class="card">
-            <div class="card-header">
+            <!-- <div class="card-header">
                 <h5 class="card-title mb-0">Upload</h5>
-            </div>
+            </div> -->
             <div class="card-body">
             
             <div class="col-12 tableFixHead">

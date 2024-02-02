e6c9fbfe87a0e7e2267d2eeaf0b743f7b77a506c
bukantio <andantiokorah@gmail.com>
2024-02-02 08:04:30 +0800

    perbaikan

--- application/views/user/V_PegawaiPensiunItem.php
+++ application/views/user/V_PegawaiPensiunItem.php
@@ -29,6 +29,7 @@
                     <th style="width: 10%;" class="text-center">Eselon</th>
                     <th style="width: 10%;" class="text-center">Jenis Jabatan</th>
                     <th style="width: 30%;" class="text-center">Unit Kerja</th>
+                    <th style="width: 10%;" class="text-center">Umur</th>
                     <th style="width: 10%;" class="text-center">TMT Pensiun</th>
                 </thead>
                 <tbody>
@@ -44,6 +45,7 @@
                             <td class="text-center"><?=($rs['eselon'])?></td>
                             <td class="text-center"><?=($rs['jenis_jabatan'])?></td>
                             <td class="text-left"><?=($rs['nm_unitkerja'])?></td>
+                            <td class="text-center"><?=($rs['umur'])?></td>
                             <td class="text-center"><?=formatDateNamaBulan($rs['tmt_pensiun'])?></td>
                         </tr>
                     <?php } ?>

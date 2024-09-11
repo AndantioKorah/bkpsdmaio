fffa68ca39878df4d0bfe3d3577ef345efef0c14
bukantio <andantiokorah@gmail.com>
2024-09-11 15:59:56 +0800

    perbaikan

---
+++ application/views/login/V_Announcement.php
@@ -0,0 +1,5 @@
+<div class="col-lg-12" style="margin: auto;">
+  <center>
+    <img id="announcement_image" style="max-height: 75vh; max-width: 90vw;" src="<?=base_url($announcement[0]['url_file'])?>" />
+  </center>
+</div>

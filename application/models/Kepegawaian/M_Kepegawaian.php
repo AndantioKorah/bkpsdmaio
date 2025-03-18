f6dca848f8f42443911fe44e715272df80cef876
Youri <torehyouri@gmail.com>
2025-03-18 15:21:54 +0800

    pull

--- application/models/Kepegawaian/M_Kepegawaian.php
+++ application/models/Kepegawaian/M_Kepegawaian.php
@@ -11089,6 +11089,7 @@ public function getFileForVerifLayanan()
                 ->get()->row_array();
             
             $datains["dokumen_layanan"] = $data['file_name'];
+            $datains["status"] = 3;
             $url_file = "arsipperbaikandata/".$data['nama_file'];
             $this->db->where('id', $id)
             ->update('t_layanan', $datains);

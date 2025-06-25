fb72db52c9ddb2afe276da6f2b595dedf3e6224b
Youri <torehyouri@gmail.com>
2025-06-25 14:29:53 +0800

    pull

--- application/models/Kepegawaian/M_Kepegawaian.php
+++ application/models/Kepegawaian/M_Kepegawaian.php
@@ -5783,6 +5783,12 @@ public function submitEditJabatan(){
         if($this->general_library->isHakAkses('verifikasi_peningkatan_penambahan_gelar')){
             $id_layanan[] = 21;
         }
+        if($this->general_library->isHakAkses('verifikasi_layanan_suket_pidana_hukdis')){
+            $id_layanan[] = 23;
+        }
+         if($this->general_library->isHakAkses('verifikasi_layanan_suket_tidak_tubel')){
+            $id_layanan[] = 24;
+        }
 
         }
 

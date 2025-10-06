e616c6a318a537ccbc0a229cae678602a35ca7a6
bukantio <andantiokorah@gmail.com>
2025-10-06 10:18:51 +0800

    perbaikan

--- application/models/rekap/M_Rekap.php
+++ application/models/rekap/M_Rekap.php
@@ -1269,7 +1269,7 @@
         $id_unitkerja == 3020000){ //diskop, kasub sudah pensiun
             $result['kasubag'] = $result['sek'];
         } else if($id_unitkerja == 4014000){
-            $result['kasubag'] = $result['sek']; // ksebang, kasub ubah jadi sek karena kasubnya ba jadi
+            $result['kasubag'] = $result['sek']; // kesbang, kasub ubah jadi sek karena kasubnya ba jadi
         } else if($id_unitkerja == 3028000){ // dinas pangan, kasub dan sek sudah pensiun ganti jadi kabid
             $result['kasubag'] = $this->db->select('a.nipbaru, a.nama, a.gelar1, a.gelar2, b.nm_pangkat, a.tmtpangkat, a.tmtcpns, d.nm_unitkerja, a.nipbaru_ws,
                                     e.id as id_m_user, a.flag_bendahara, e.nama_jabatan, e.kepalaskpd')

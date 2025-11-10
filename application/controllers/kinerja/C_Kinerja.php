4288297ab46c1913bcb21afb682fdf1dd5825aa1
Youri Toreh <torehyouri@gmail.com>
2025-11-10 18:07:53 +0800

    pll

--- application/controllers/kinerja/C_Kinerja.php
+++ application/controllers/kinerja/C_Kinerja.php
@@ -528,7 +528,7 @@ class C_Kinerja extends CI_Controller
         $date = new DateTime();
         $date->modify("last day of previous month");
         $data['maxDate'] = countMaxDateUpload2(formatDateOnlyForEdit($date->format("Y-m-d")), 0, 'minus');
-        dd($data['maxDate']['max_date']);
+        // dd($data['maxDate']['max_date']);
         $data['skpd'] = $this->master->getAllUnitKerja();
         $data['pegawai'] = $this->kinerja->getPegawaiPeninjauanAbsensi();
         render('kinerja/V_PeninjauanAbsensi', '', '', $data);
